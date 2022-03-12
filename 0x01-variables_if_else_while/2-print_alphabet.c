94d7597e8c28071a2291e626aad16b036271348b
aisha <aishatadebayor0@gmail.com>
2022-03-12 01:16:22 -0800

    execute

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase
- * followed by a new line
+ * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
+	int n;
 
-	for (ch='a'; ch <= 'z'; ch++)
+	for (n = 48; n < 58; n++)
 	{
-	putchar(ch);
+	putchar(n);
+	if (n != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
-}	
+}
