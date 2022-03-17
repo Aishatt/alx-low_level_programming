db44c71aa7751471688713be77e5cdd6a961aace
aisha <aishatadebayor0@gmail.com>
2022-03-17 15:39:10 -0700

    more on nested loop

---
+++ 2-mul.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * mul- multiplies 2 integers
+ * @a : int to be multiplied
+ * @b: int to be multiplied to a
+ * return: the result of the operation
+ */
+int mul (int a , int b)
+{
+	int c;
+
+	c = a*b;
+	return (c);
+}	
