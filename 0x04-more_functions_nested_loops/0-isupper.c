07ac44dc647a16e0c913cb422029e9e7d0cb1d12
aisha <aishatadebayor0@gmail.com>
2022-03-17 20:36:03 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * a function that checks for uppercase character
+ * returns 1 if c is uppercase
+ * return 0 if otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}	
