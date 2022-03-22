aad26c4de920a076322fbdf9f925773da17ac9fe
aisha <aishatadebayor0@gmail.com>
2022-03-21 17:49:00 -0700

    correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,11 +5,11 @@
  * main- check the code
  * function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
+ * @n: input integer
  * Return: Always 0.
  */
 
 void reset_to_98(int *n)
 {
-	
 	*n = 98;
 }
