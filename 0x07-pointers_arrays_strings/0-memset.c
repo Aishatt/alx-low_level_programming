f2accee7658a9fcd734a111e3803dddd8d39c77f
aisha <aishatadebayor0@gmail.com>
2022-03-28 22:26:26 -0700

    pointer to pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,10 @@
 #include "main.h"
 /**
  * _memset - function that prints the first bytes
- * @s:pointer
+ * @s:pointer,memory area
  * @b: character variable
  * @n: unisigned int
+ * Return: the pointer to dest.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
