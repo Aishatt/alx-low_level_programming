e33aad436b31639bd2621cdf8db2f9b33f05edde
aisha <aishatadebayor0@gmail.com>
2022-04-02 00:34:44 -0700

    correction

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - prints all arguements it receives.
- * @argc: number of command line arguements.
- * @argv: array that contains the command line arguements.
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc , char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
-	{
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-	}
-	return 0;
+	return (0);
 }
